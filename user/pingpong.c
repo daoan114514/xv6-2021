f9ae205d8819d4317c4e6a4e86329b0a49353e82
EdmundMC <1743220021@qq.com>
2023-10-01 17:23:19 +0800

    modified pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -8,17 +8,17 @@ int main()
     char buf[2];
     pipe(p);
     if(!fork()){
+        close(p[1]);
         read(p[0], buf, 1);
         printf("%d: received ping\n", getpid());
         write(p[0], "\n", 1);
         close(p[0]);
-        close(p[1]);
     } else {
-        write(p[1], "\n", 1);
-        read(p[1], buf, 1);
-        wait(0);
-        printf("%d: received pong\n", getpid());
         close(p[0]);
+        write(p[1], "\n", 1);
+        wait(0);
+        read(p[1], buf, 1);
+        printf("%d: received pong\n", getpid());
         close(p[1]);
     }
     exit(0);
